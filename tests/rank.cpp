ec63346b6d784cc081a5ea9fa6b1fd68bc5deb9b
Robert Adam <dev@robert-adam.de>
2026-04-24 21:51:51 +0200

    Fix wrong dims in test

--- tests/rank.cpp
+++ tests/rank.cpp
@@ -74,7 +74,7 @@ INSTANTIATE_TEST_SUITE_P(
 		RankTest::param_tuple({ 0, 0, 1, 0 }, util::make_info({ 3, 3, 3, 3 }, { { { 2, 3 } }, { { 0, 1 } } }), 1),
 		RankTest::param_tuple({ 0, 0, 0, 1 }, util::make_info({ 3, 3, 3, 3 }, { { { 3, 2 } }, { { 0, 1 } } }), 1),
 		RankTest::param_tuple({ 0, 0, 1, 0 }, util::make_info({ 3, 5, 2, 2 }, { { { 0 }, { 1 } }, { { 2, 3 } } }), 15),
-		RankTest::param_tuple({ 0, 1, 2, 2 }, util::make_info({ 3, 5, 2, 2 }, { { { 0 }, { 1 } }, { { 2, 3 } } }), 78)
+		RankTest::param_tuple({ 0, 1, 2, 2 }, util::make_info({ 3, 5, 3, 3 }, { { { 0 }, { 1 } }, { { 2, 3 } } }), 78)
 	)
 );
 // clang-format on
