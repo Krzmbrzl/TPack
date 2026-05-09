08ecf0fd21d4a661d3d46ed750cf96e5f6fe500f
Robert Adam <dev@robert-adam.de>
2026-05-09 20:57:37 +0200

    Add (un)rank overloads that avoid memory allocations

--- include/tpack/rank.hpp
+++ include/tpack/rank.hpp
@@ -13,9 +13,9 @@
 
 namespace tpack {
 
-template< std::ranges::random_access_range Indexing, std::ranges::random_access_range Dimensions,
-		  std::ranges::range Partitions >
-constexpr std::size_t rank(Indexing &&idx, Dimensions &&dims, Partitions &&parts) {
+template< std::ranges::random_access_range Indexing, std::ranges::random_access_range EffectiveIndexing,
+		  std::ranges::random_access_range Dimensions, std::ranges::range Partitions >
+constexpr std::size_t rank(Indexing &&idx, Dimensions &&dims, Partitions &&parts, EffectiveIndexing &&effective_idx) {
 	using std::ranges::begin;
 	using std::ranges::end;
 	using std::ranges::size;
@@ -25,16 +25,15 @@ constexpr std::size_t rank(Indexing &&idx, Dimensions &&dims, Partitions &&parts
 	assert(is_canonical(idx, parts));
 	assert(size(idx) == size(dims));
 
-	std::vector< std::size_t > effective_idx;
-
 	std::size_t stride = 1;
 	for (auto &&part_levels : parts) {
 		// Convert into effective 1D partition (merge different partition levels)
-		effective_idx.resize(size(*begin(part_levels)));
+		const std::size_t effective_size = size(*begin(part_levels));
+		assert(effective_idx.size() >= effective_size);
 		std::ranges::fill(effective_idx, 0);
 
 		std::size_t effective_dim = 0;
-		for (std::size_t k = 0; k < effective_idx.size(); ++k) {
+		for (std::size_t k = 0; k < effective_size; ++k) {
 			std::size_t col_stride = 1;
 			for (auto &&current : part_levels) {
 				effective_idx[k] += col_stride * idx[current[k]];
@@ -47,11 +46,11 @@ constexpr std::size_t rank(Indexing &&idx, Dimensions &&dims, Partitions &&parts
 		}
 
 		// If effective_idx is not sorted, idx wasn't canonical
-		assert(std::ranges::is_sorted(effective_idx, std::greater<>{}));
+		assert(std::ranges::is_sorted(effective_idx | std::ranges::views::take(effective_size), std::greater<>{}));
 
 		// Compute rank of that effective 1D partition
 		std::size_t current_rank = 0;
-		for (std::size_t i = 0; i < effective_idx.size(); ++i) {
+		for (std::size_t i = 0; i < effective_size; ++i) {
 			if (effective_idx[i] == 0) {
 				// All subsequent elements must also be zero since we are dealing with
 				// a canonical indexing that guarantees to list elements in non-increasing
@@ -60,7 +59,7 @@ constexpr std::size_t rank(Indexing &&idx, Dimensions &&dims, Partitions &&parts
 			}
 
 			const std::size_t n = effective_idx[i];
-			const std::size_t k = effective_idx.size() - i;
+			const std::size_t k = effective_size - i;
 			current_rank += details::binomial(n + k - 1, k);
 		}
 
@@ -68,25 +67,34 @@ constexpr std::size_t rank(Indexing &&idx, Dimensions &&dims, Partitions &&parts
 		rank += stride * current_rank;
 
 		// Update stride by multiplying it by the max rank of the effective 1D partition
-		stride *= details::binomial(effective_dim + effective_idx.size() - 1, effective_idx.size());
+		stride *= details::binomial(effective_dim + effective_size - 1, effective_size);
 	}
 
 	return rank;
 }
 
+template< std::ranges::random_access_range Indexing, std::ranges::random_access_range Dimensions,
+		  std::ranges::range Partitions >
+std::size_t rank(Indexing &&idx, Dimensions &&dims, Partitions &&parts) {
+	using std::ranges::begin;
+	using std::ranges::size;
+
+	static thread_local std::vector< std::size_t > effective_idx;
+	effective_idx.resize(size(idx));
+
+	return rank(idx, dims, parts, effective_idx);
+}
 
 
-template< std::ranges::random_access_range Indexing = std::vector< std::size_t >,
+
+template< std::ranges::random_access_range Indexing, std::ranges::random_access_range EffectiveIndexing,
 		  std::ranges::random_access_range Dimensions, std::ranges::range Partitions >
-constexpr Indexing unrank(std::size_t rank, Dimensions &&dims, Partitions &&parts) {
+constexpr void unrank(Indexing &&idx, std::size_t rank, Dimensions &&dims, Partitions &&parts,
+					  EffectiveIndexing &&effective_idx) {
 	using std::ranges::begin;
 	using std::ranges::end;
 	using std::ranges::size;
 
-	Indexing idx;
-	if constexpr (requires(Indexing i) { i.resize(5); }) {
-		idx.resize(size(dims));
-	}
 	assert(size(idx) == size(dims));
 
 	std::ranges::fill(idx, 0);
@@ -126,8 +134,9 @@ constexpr Indexing unrank(std::size_t rank, Dimensions &&dims, Partitions &&part
 		rank -= current_rank * part_strides[part_idx];
 
 		// Unrank current_rank into effective indexing
-		std::vector< std::size_t > effective_idx;
-		effective_idx.resize(size(*begin(part_levels)), 0);
+		[[maybe_unused]] const std::size_t effective_size = size(*begin(part_levels));
+		assert(effective_idx.size() >= effective_size);
+		std::ranges::fill(effective_idx, 0);
 
 		std::size_t num_cols = size(*begin(part_levels));
 
@@ -176,7 +185,7 @@ constexpr Indexing unrank(std::size_t rank, Dimensions &&dims, Partitions &&part
 
 		// In order for a 1D indexing consisting of only a single partition to be
 		// canonical, it has to be sorted
-		assert(std::ranges::is_sorted(effective_idx, std::greater<>{}));
+		assert(std::ranges::is_sorted(effective_idx | std::ranges::views::take(effective_size), std::greater<>{}));
 
 
 		// Transform effective indexing into actual indexing by splitting
@@ -205,6 +214,27 @@ constexpr Indexing unrank(std::size_t rank, Dimensions &&dims, Partitions &&part
 	}
 
 	assert(is_canonical(idx, parts));
+}
+
+template< std::ranges::random_access_range Indexing, std::ranges::random_access_range Dimensions,
+		  std::ranges::range Partitions >
+void unrank(Indexing &&idx, std::size_t rank, Dimensions &&dims, Partitions &&parts) {
+	using std::ranges::begin;
+	using std::ranges::size;
+
+	static thread_local std::vector< std::size_t > effective_idx;
+	effective_idx.resize(size(dims));
+
+	unrank(idx, rank, dims, parts, effective_idx);
+}
+
+template< std::ranges::random_access_range Indexing = std::vector< std::size_t >,
+		  std::ranges::random_access_range Dimensions, std::ranges::range Partitions >
+Indexing unrank(std::size_t rank, Dimensions &&dims, Partitions &&parts) {
+	Indexing idx;
+	idx.resize(size(dims));
+
+	unrank(idx, rank, dims, parts);
 
 	return idx;
 }
